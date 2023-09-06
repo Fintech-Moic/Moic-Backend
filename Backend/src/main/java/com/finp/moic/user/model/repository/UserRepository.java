70fdc4f26672ca35ba89844c55a9978b1489548b
임혜지 <hyeji4775@naver.com>
2023-09-06 11:26:19 +0900

    Fix: Frontend Branch Setting

--- Backend/src/main/java/com/finp/moic/user/model/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.finp.moic.user.model.repository;
-
-import com.finp.moic.user.model.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Long> {
-}
--- Backend/src/main/java/com/finp/moic/util/exception/AlreadyExistException.java
+++
@@ -1,8 +0,0 @@
-package com.finp.moic.util.exception;
-
-public class AlreadyExistException extends BusinessException{
-
-    public AlreadyExistException(ExceptionEnum e) {
-        super(e);
-    }
-}

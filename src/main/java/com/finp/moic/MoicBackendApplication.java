dfcd91852dae4ef1b91aeae67b1d02440a32f7ee
LimHyeji <ihj028003@gmail.com>
2023-09-06 10:53:14 +0900

    Feat: 프로젝트 세팅

---
+++ src/main/java/com/finp/moic/MoicBackendApplication.java
@@ -0,0 +1,13 @@
+package com.finp.moic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoicBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoicBackendApplication.class, args);
+	}
+
+}

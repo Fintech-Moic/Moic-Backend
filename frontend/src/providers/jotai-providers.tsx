81032bb86d78a75ff30ff25400b094845e41ef6c
배희진 <dilook0424@naver.com>
2023-09-11 14:36:54 +0900

    Feat: Jotai, Tailwind CSS 세팅

--- frontend/src/providers/jotai-providers.tsx
+++ frontend/src/providers/jotai-providers.tsx
@@ -4,8 +4,6 @@ import React from 'react';
 import { Provider as JotaiProvider } from 'jotai';
 import { atom } from 'jotai';
 
-export const countAtom = atom(0);
-
 export function JotaiWrapper({ children }: { children: React.ReactNode }) {
   return (
     <JotaiProvider>

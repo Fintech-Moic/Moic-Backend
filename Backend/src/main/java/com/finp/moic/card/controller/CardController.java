58084437b69df3b7e6ea1c0c0654a3e49974c295
LimHyeji <ihj028003@gmail.com>
2023-09-11 14:33:17 +0900

    Fix: 서비스 인터페이스 구조로 변경

--- Backend/src/main/java/com/finp/moic/card/controller/CardController.java
+++ Backend/src/main/java/com/finp/moic/card/controller/CardController.java
@@ -2,7 +2,7 @@ package com.finp.moic.card.controller;
 
 import com.finp.moic.card.model.dto.response.CardRegistResponseDTO;
 import com.finp.moic.card.model.dto.request.CardRegistRequestDTO;
-import com.finp.moic.card.model.service.CardService;
+import com.finp.moic.card.model.service.CardServiceImpl;
 import com.finp.moic.util.dto.ResponseDTO;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,18 +15,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CardController {
 
-    private final CardService cardService;
+    private final CardServiceImpl cardServiceImpl;
 
     @Autowired
-    public CardController(CardService cardService) {
-        this.cardService = cardService;
+    public CardController(CardServiceImpl cardServiceImpl) {
+        this.cardServiceImpl = cardServiceImpl;
     }
 
     /* 혜지 : 인덱스 테스트 코드. 추후 삭제 예정 */
     @PostMapping("/card/regist")
     public ResponseEntity<ResponseDTO> registCard(@RequestBody @Valid CardRegistRequestDTO cardRegistRequestDTO){
 
-        CardRegistResponseDTO response=cardService.registCard(cardRegistRequestDTO);
+        CardRegistResponseDTO response= cardServiceImpl.registCard(cardRegistRequestDTO);
 
         return ResponseEntity.status(HttpStatus.OK).body(ResponseDTO.builder()
                         .message("인덱스 테스트 완료")
--- Backend/src/main/java/com/finp/moic/card/model/service/CardService.java
+++ Backend/src/main/java/com/finp/moic/card/model/service/CardService.java
@@ -2,50 +2,8 @@ package com.finp.moic.card.model.service;
 
 import com.finp.moic.card.model.dto.request.CardRegistRequestDTO;
 import com.finp.moic.card.model.dto.response.CardRegistResponseDTO;
-import com.finp.moic.card.model.entity.Card;
-import com.finp.moic.card.model.repository.CardRepository;
-import com.finp.moic.util.exception.ExceptionEnum;
-import com.finp.moic.util.exception.list.NotFoundException;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
-@Service
-public class CardService {
+public interface CardService {
 
-    private final CardRepository cardRepository;
-
-    @Autowired
-    public CardService(CardRepository cardRepository) {
-        this.cardRepository = cardRepository;
-    }
-
-    /* 혜지 : 인덱스 테스트 코드. 추후 삭제 예정 */
-    public CardRegistResponseDTO registCard(CardRegistRequestDTO cardRegistRequestDTO) {
-
-        /*** Entity Builder ***/
-        Card card=Card
-                .builder()
-                .company(cardRegistRequestDTO.getCompany())
-                .type(cardRegistRequestDTO.getType())
-                .name(cardRegistRequestDTO.getName())
-                .cardImage(cardRegistRequestDTO.getCardImage())
-                .build();
-
-        /*** RDB Access ***/
-        cardRepository.save(card);
-
-        cardRepository.findByName(card.getName()).orElseThrow(()->new NotFoundException(ExceptionEnum.CARD_LOAD_ERROR));
-
-        /*** DTO Builder ***/
-        CardRegistResponseDTO dto=CardRegistResponseDTO
-                .builder()
-                .cardSeq(card.getCardSeq())
-                .company(card.getCompany())
-                .type(card.getType())
-                .name(card.getName())
-                .cardImage(card.getCardImage())
-                .build();
-
-        return dto;
-    }
+    public CardRegistResponseDTO registCard(CardRegistRequestDTO cardRegistRequestDTO);
 }

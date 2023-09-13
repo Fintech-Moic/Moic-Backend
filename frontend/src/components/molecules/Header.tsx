56edfc097e90da92c02fef77e008cc6511c70d19
suhwan2004 <suhwan2004@gmail.com>
2023-09-13 10:39:21 +0900

    Feat : 헤더 컴포넌트 내에 Router 이벤트 ,주석 추가

--- frontend/src/components/molecules/Header.tsx
+++ frontend/src/components/molecules/Header.tsx
@@ -1,3 +1,4 @@
+import { useRouter } from 'next/router';
 import IconButton from '../atoms/IconButton';
 import WhiteRightArrowIcon from '@/../public/assets/WhiteRightArrowIcon.svg';
 import WhiteFilterIcon from '@/../public/assets/WhilteFilterIcon.svg';
@@ -8,21 +9,39 @@ interface HeaderProps {
   isFilterButton: boolean;
 }
 
+/** 페이지 상단에 배치되는 Header Component
+ * @param {String} title Header의 가운데에 배치되는 메인 제목
+ * @param {Boolean} isPrevButton 뒤로 돌아가기 버튼의 렌더링 여부
+ * @param {Boolean} isFilterButton 필터링 버튼의 렌더링 여부
+ * @returns {JSX.Element} Header Component 반환
+ */
+
 export default function Header({
   title,
   isPrevButton,
   isFilterButton,
 }: HeaderProps) {
+  const router = useRouter();
   return (
     <header className="w-100% h-14 bg-[#FFBF69] flex flex-row justify-between items-center gap-2 pr-2 pl-2">
       {isPrevButton ? (
-        <IconButton src={WhiteRightArrowIcon} onClick={() => {}} />
+        <IconButton
+          src={WhiteRightArrowIcon}
+          onClick={() => {
+            router.back();
+          }}
+        />
       ) : (
         <div />
       )}
       <span className="text-[20px]">{title}</span>
       {isFilterButton ? (
-        <IconButton src={WhiteFilterIcon} onClick={() => {}} />
+        <IconButton
+          src={WhiteFilterIcon}
+          onClick={() => {
+            // fix me! 추후, Card 구현 시 로직 추가
+          }}
+        />
       ) : (
         <div />
       )}

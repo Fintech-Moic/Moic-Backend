57b1426317e7add4dce8c346a6d631f0cd25cbc0
LimHyeji <ihj028003@gmail.com>
2023-09-18 17:30:38 +0900

    Feat: 카드 등록 및 삭제 API 보완

--- Backend/src/main/java/com/finp/moic/card/model/service/CardServiceImpl.java
+++ Backend/src/main/java/com/finp/moic/card/model/service/CardServiceImpl.java
@@ -13,6 +13,7 @@ import com.finp.moic.user.model.repository.UserRepository;
 import com.finp.moic.util.database.config.RedisConfig;
 import com.finp.moic.util.database.service.RedisService;
 import com.finp.moic.util.exception.ExceptionEnum;
+import com.finp.moic.util.exception.list.AlreadyExistException;
 import com.finp.moic.util.exception.list.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -45,22 +46,26 @@ public class CardServiceImpl implements CardService {
     @Override
     public void registCard(CardRegistRequestDTO cardRegistRequestDTO, String userId) {
 
-        /*** Validation ***/
-        
         /**
-         * TO DO :: 중복된 카드에 대해 저장되지 않도록 처리 (삭제에서 예외 발생)
+         * TO DO :: SOFT DELETE 확인해, 존재 시 회복하기
          * */
-        
+
+        /*** Validation ***/
         Card card=cardRepository.findByName(cardRegistRequestDTO.getCardName())
                 .orElseThrow(()->new NotFoundException(ExceptionEnum.CARD_NOT_FOUND));
         User user=userRepository.findById(userId)
                 .orElseThrow(()->new NotFoundException(ExceptionEnum.USER_NOT_FOUND));
 
-        /**
-         * TO DO :: SOFT DELETE 확인해, 존재 시 회복하기
-         * */
+        /* 혜지 : 한 사용자에 대해 중복된 카드 등록 불가 */
+        List<Card> cardList=userCardRepository.findAllByUserId(userId);
+        for(Card userCard:cardList){
+            if(card.getName().equals(userCard.getName())){
+                throw new AlreadyExistException(ExceptionEnum.CARD_REGIST_DUPLICATE);
+            }
+        }
 
         /*** RDB Access ***/
+        /* 혜지 : userCardSeq 등의 기본 데이터셋 저장 */
         UserCard userCard=userCardRepository.save(UserCard.builder()
                 .build());
 
@@ -72,6 +77,7 @@ public class CardServiceImpl implements CardService {
                 .build();
 
         /*** RDB Access ***/
+        /* 혜지 : userCard FK 저장 */
         userCardRepository.save(userCard);
 
     }
@@ -154,7 +160,6 @@ public class CardServiceImpl implements CardService {
         /*** Validation ***/
         UserCard userCard=userCardRepository.findByCardName(cardDeleteRequestDTO.getCardName())
                 .orElseThrow(()->new NotFoundException(ExceptionEnum.CARD_USER_NOT_FOUND));
-
         User user=userRepository.findById(userId)
                 .orElseThrow(()->new NotFoundException(ExceptionEnum.USER_NOT_FOUND));
 

562400b755cf30abe09be3990dd2e9a7bfeb5bd8
suhwan2004 <suhwan2004@gmail.com>
2023-09-19 17:32:46 +0900

    Fix: profit layout, card page 상단 필터 틀 구현

--- frontend/src/app/profit/card/page.tsx
+++ frontend/src/app/profit/card/page.tsx
@@ -1,3 +1,14 @@
-export default function page() {
-  return <div className="t1">card</div>;
+'use client';
+
+import { useAtomValue } from 'jotai';
+import ProfitFilter from '../molecules/ProfitFilter';
+import { filterOpenAtom } from '@/store/atoms/header';
+
+export default function Page() {
+  const filterOpen = useAtomValue(filterOpenAtom);
+  return (
+    <div className="w-full h-full relative">
+      {filterOpen && <ProfitFilter />}
+    </div>
+  );
 }

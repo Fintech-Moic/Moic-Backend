6a5d5cfc86e1a77c5565f91e9414a4af67ba1101
김성재 <tjdwo6269@naver.com>
2023-09-19 15:12:59 +0900

    Feat: 자동완성 전용 Redis Connection 연결 및 자동완성 설정

--- Backend/src/main/java/com/finp/moic/util/database/service/RedisService.java
+++ Backend/src/main/java/com/finp/moic/util/database/service/RedisService.java
@@ -14,25 +14,28 @@ import java.util.concurrent.TimeUnit;
 @Service
 public class RedisService {
 
-    /* 성재 : Redis 서버 2개 분리 */
+    /* 성재 : Redis 서버 3개 분리 */
 
     /**
-     * 모든 데이터가 저장되있는 Redis 접근 : redisTemplate1
+     * 모든 데이터가 저장되있는 Redis 접근 : MainRedis
      */
-    private final RedisTemplate<String, Object> redisTemplate1;
+    private final RedisTemplate<String, Object> mainRedis;
 
     /**
-     * Refresh Token만 저장되있는 Redis 접근 : redisTemplate2
+     * Refresh Token만 저장되있는 Redis 접근 : SubRedis
      */
-    private final RedisTemplate<String, String> redisTemplate2; //
+    private final RedisTemplate<String, String> securityRedis; //
+
+    private final RedisTemplate<String, String> autoRedis;
 
     @Autowired
-    public RedisService(@Qualifier("redisTemplate1") RedisTemplate redisTemplate1,
-                                 @Qualifier("redisTemplate2") RedisTemplate redisTemplate2) {
-        this.redisTemplate1=redisTemplate1;
-        this.redisTemplate2=redisTemplate2;
+    public RedisService(@Qualifier("MainRedis") RedisTemplate<String, Object> mainRedis,
+                        @Qualifier("SecurityRedis") RedisTemplate<String, String> securityRedis,
+                        @Qualifier("AutoRedis") RedisTemplate<String, String> autoRedis) {
+        this.mainRedis = mainRedis;
+        this.securityRedis = securityRedis;
+        this.autoRedis = autoRedis;
     }
-
 
     /***** [Auto Complete] *****/
 
@@ -45,21 +48,21 @@ public class RedisService {
      *  내 카드 저장
      *  **/
     public void setUserCard(String userId, String cardName){
-        redisTemplate1.opsForList().rightPush(setUserCardKey(userId),cardName);
+        mainRedis.opsForList().rightPush(setUserCardKey(userId),cardName);
     }
 
     /**
      * 내 카드 목록 한번에 저장 (캐싱 데이터 날아가는 상황 발생 시 이용)
      *  **/
     public void setUserCardList(String userId, List<CardMineResponseDTO> userCardList){
-        redisTemplate1.opsForList().rightPushAll(setUserCardKey(userId),userCardList);
+        mainRedis.opsForList().rightPushAll(setUserCardKey(userId),userCardList);
     }
 
     /**
      * 내 카드 목록 조회
      * **/
     public List getUserCardList(String userId){
-        return redisTemplate1.opsForList().range(setUserCardKey(userId),0,-1);
+        return mainRedis.opsForList().range(setUserCardKey(userId),0,-1);
     }
 
     /***** [User CardBenefits] *****/
@@ -70,13 +73,13 @@ public class RedisService {
 
     /***** [Refresh Token] *****/
     public void setRefreshToken(String userId, String refreshToken){
-        redisTemplate2.opsForValue().set(refreshToken,userId);
+        securityRedis.opsForValue().set(refreshToken,userId);
         //일단 60초
-        redisTemplate2.expire(refreshToken,60L, TimeUnit.SECONDS);
+        securityRedis.expire(refreshToken,60L, TimeUnit.SECONDS);
     }
 
     public String getRefreshToken(String refreshToken){
-        return redisTemplate2.opsForValue().get(refreshToken);
+        return securityRedis.opsForValue().get(refreshToken);
     }
 
 }

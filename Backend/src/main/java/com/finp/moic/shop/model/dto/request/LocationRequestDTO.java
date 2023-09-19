eac586bacdb710d83ea816acc32213d41c5bad56
LimHyeji <ihj028003@gmail.com>
2023-09-19 15:18:18 +0900

    Feat: 테스트용 DTO 생성

--- Backend/src/main/java/com/finp/moic/shop/model/dto/request/LocationRequestDTO.java
+++ Backend/src/main/java/com/finp/moic/shop/model/dto/request/LocationRequestDTO.java
@@ -1,9 +1,19 @@
 package com.finp.moic.shop.model.dto.request;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
 
 @Getter
 @ToString
 public class LocationRequestDTO {
+
+    private double latitude;
+    private double longitude;
+
+    @Builder
+    public LocationRequestDTO(double latitude, double longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
 }
--- Backend/src/main/java/com/finp/moic/shop/model/dto/response/LocationResponseDTO.java
+++ Backend/src/main/java/com/finp/moic/shop/model/dto/response/LocationResponseDTO.java
@@ -1,9 +1,26 @@
 package com.finp.moic.shop.model.dto.response;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
 
 @Getter
 @ToString
 public class LocationResponseDTO {
+
+    private String shopName;
+    private String address;
+    private double latitude;
+    private double longitude;
+    private double distance;
+
+    @Builder
+    public LocationResponseDTO(String shopName, String address,
+                               double latitude, double longitude, double distance) {
+        this.shopName = shopName;
+        this.address = address;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.distance = distance;
+    }
 }

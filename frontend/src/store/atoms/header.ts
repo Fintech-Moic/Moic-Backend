f7a5844952ac484a376283bf85354d98859769ff
suhwan2004 <suhwan2004@gmail.com>
2023-09-19 17:31:45 +0900

    Feat: store 내에 header atoms 추가

---
+++ frontend/src/store/atoms/header.ts
@@ -0,0 +1,6 @@
+import { atom } from 'jotai';
+
+export const titleAtom = atom('');
+export const prevExistAtom = atom(false);
+export const filterOpenAtom = atom(false);
+export const filterExistAtom = atom(false);

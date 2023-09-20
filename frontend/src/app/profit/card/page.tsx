2008da9b15b863e121836d9a8f8a94a8c4fa6189
suhwan2004 <suhwan2004@gmail.com>
2023-09-20 12:58:40 +0900

    Feat: profit/card 페이지 틀 작성

--- frontend/src/app/profit/card/page.tsx
+++ frontend/src/app/profit/card/page.tsx
@@ -1,14 +1,20 @@
 'use client';
 
 import { useAtomValue } from 'jotai';
+import { useState } from 'react';
 import ProfitFilter from '../organisms/ProfitFilter';
 import { filterOpenAtom } from '@/store/atoms/header';
+import Pagination from '@/components/molecules/Pagination';
 
+// fix me! : 카드 리스트 렌더링 필요
 export default function Page() {
   const filterOpen = useAtomValue(filterOpenAtom);
+  const [idx, setIdx] = useState(0);
   return (
     <div className="w-full h-full relative">
       {filterOpen && <ProfitFilter />}
+      <div className="" />
+      <Pagination idx={idx} setIdx={setIdx} pageLength={10} />
     </div>
   );
 }

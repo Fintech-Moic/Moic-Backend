e6ecddde5b2087b8162d3736e165df9aa7688a77
suhwan2004 <suhwan2004@gmail.com>
2023-09-20 10:12:57 +0900

    Feat: SearchInput 컴포넌트 추가

---
+++ frontend/src/app/profit/atoms/SearchInput.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+export default function SearchInput() {
+  return (
+    <input
+      type="text"
+      name="search"
+      className="p2r w-full h-12 border-[#9BA5B7] rounded-[10px] border-2 pr-8 pl-2 focus:outline-none"
+    />
+  );
+}

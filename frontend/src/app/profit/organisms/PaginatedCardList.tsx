08eb865c82999101c150f999309a19988c5decc5
suhwan2004 <suhwan2004@gmail.com>
2023-09-21 17:59:09 +0900

    Fix: CardListItem에 나의 카드 표시 추가

--- frontend/src/app/profit/organisms/PaginatedCardList.tsx
+++ frontend/src/app/profit/organisms/PaginatedCardList.tsx
@@ -7,7 +7,7 @@ import Pagination from '@/components/molecules/Pagination';
 export default function PaginatedCardList({ data }: any) {
   const [idx, setIdx] = useState(0);
   return (
-    <div className="flex flex-col items-center h-full pb-6 justify-end gap-8">
+    <div className="flex flex-col items-center h-full pb-6 justify-between gap-8 pt-14">
       <CardList list={data.cardList} currentPage={idx + 1} />
       <Pagination
         idx={idx}

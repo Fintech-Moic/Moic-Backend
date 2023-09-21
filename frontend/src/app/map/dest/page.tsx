b93bc9eee3be0b0bab0607f769986cd02f849f5e
배희진 <dilook0424@naver.com>
2023-09-21 17:25:57 +0900

    Feat: 자동입력 테스트

--- frontend/src/app/map/dest/page.tsx
+++ frontend/src/app/map/dest/page.tsx
@@ -1,3 +1,7 @@
+'use client';
+
+import React, { useState } from 'react';
+
 export async function getSearchData() {
   const res = await fetch(`https://moic.site/api/v1/autocomplete?word=안경`, {
     method: 'POST',
@@ -5,6 +9,34 @@ export async function getSearchData() {
   const data = await res.json()
 
   return data;
+}
+
+export async function listOfSearch() {
+
+  const posts = await getSearchData();
+  console.log('SEARCH DATA CHECK ===>', posts)
+
+  return posts
+}
+
+export default function searchShop() {
+
+  let [inputValue, setInputValue] = useState('');
+  const onChangeInput = (e: any) => {
+    inputValue = e.target.value
+    console.log(inputValue)
+  }
+
+  return (
+    <div>
+      <input
+        id="inputValue"
+        name="inputValue"
+        placeholder="검색어를 입력하세요"
+        onChange={onChangeInput}
+      />
+    </div>
+  )
 }
 
 // export default async function ListOfSearch() {
@@ -24,16 +56,3 @@ export async function getSearchData() {
 //     </div>
 //   )
 // }
-
-export async function listOfSearch() {
-  const posts = await getSearchData();
-  console.log('SEARCH DATA CHECK ===>', posts)
-
-  return posts
-}
-
-export default function searchShop() {
-  return (
-    <div></div>
-  )
-}

b9224662aa5cdbe83b140a7a4ca03b42fa69f92f
suhwan2004 <suhwan2004@gmail.com>
2023-09-21 16:24:09 +0900

    Fix: filterOptionAtom 추가

--- frontend/src/store/atoms/header.ts
+++ frontend/src/store/atoms/header.ts
@@ -4,3 +4,4 @@ export const titleAtom = atom('');
 export const prevExistAtom = atom(false);
 export const filterOpenAtom = atom(false);
 export const filterExistAtom = atom(false);
+export const filterOptionAtom = atom({ company: '', type: '', cardName: '' });

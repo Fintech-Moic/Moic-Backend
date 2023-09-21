99996a57a1d62da4e3b9fd0b758b89e584971f7a
suhwan2004 <suhwan2004@gmail.com>
2023-09-22 08:06:56 +0900

    Fix: FilterOptionAtom을 베이스로 잡고 있었던 필터 영역 수정 및 profit/card/page 내 ssr 적용

--- frontend/src/app/profit/organisms/PaginatedCardList.tsx
+++ frontend/src/app/profit/organisms/PaginatedCardList.tsx
@@ -1,20 +1,37 @@
 'use client';
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { useAtomValue } from 'jotai';
 import CardList from '../molecules/CardList';
 import Pagination from '@/components/molecules/Pagination';
+import { filterOptionAtom } from '@/store/atoms/header';
+import { getCardSearch } from '@/api/card';
 
 export default function PaginatedCardList({ data }: any) {
   const [idx, setIdx] = useState(0);
+  const filterOption = useAtomValue(filterOptionAtom);
+  const [searchData, setSearchData] = useState(data);
+
+  useEffect(() => {
+    (async () => {
+      const response = await getCardSearch({
+        company: filterOption.company,
+        type: filterOption.type,
+        cardName: filterOption.cardName,
+      });
+      setSearchData(response.data.cardList);
+    })();
+  }, [filterOption]);
+
   return (
     <div className="flex flex-col items-center h-full pb-6 justify-between gap-8 pt-14">
-      <CardList list={data.cardList} currentPage={idx + 1} />
+      <CardList list={searchData} currentPage={idx + 1} />
       <Pagination
         idx={idx}
         setIdx={setIdx}
         pageLength={Math.round(
           (
-            data.cardList as Array<{
+            searchData as Array<{
               cardImage: string;
               company: string;
               mine: boolean;

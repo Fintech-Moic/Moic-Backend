99c86f77a41eb996c8fe0b4391e5916cf58ed3c0
suhwan2004 <suhwan2004@gmail.com>
2023-09-21 16:23:13 +0900

    Fix: ProfitFiter 컴포넌트의 목 데이터 제거 및 서버 연결 로직 추가

--- frontend/src/app/profit/organisms/ProfitFilter.tsx
+++ frontend/src/app/profit/organisms/ProfitFilter.tsx
@@ -1,12 +1,20 @@
 'use client';
 
-import { useState, useCallback } from 'react';
+import { useState, useCallback, useEffect } from 'react';
+import { useAtomValue, useSetAtom } from 'jotai';
 import Switch from '../atoms/Switch';
 import SearchInputBar from '../molecules/SearchInputBar';
 import Dropdown from '@/components/atoms/Dropdown';
+import { filterOpenAtom, filterOptionAtom } from '@/store/atoms/header';
 
-export default function ProfitFilter() {
+interface ProfitFilterProps {
+  data: any;
+}
+
+export default function ProfitFilter({ data }: ProfitFilterProps) {
+  const filterOpen = useAtomValue(filterOpenAtom);
   const [isSwitchOn, setIsSwitchOn] = useState(false);
+  const setFilterOption = useSetAtom(filterOptionAtom);
   const [selectedCompany, setSelectedCompany] = useState<{
     id: string;
     value: string;
@@ -15,19 +23,27 @@ export default function ProfitFilter() {
     id: string;
     value: string;
   } | null>(null);
+  useEffect(() => {
+    setFilterOption((prev) => ({
+      ...(prev as any),
+      company: selectedCompany?.value,
+      type: selectedType?.value,
+    }));
+  }, [selectedCompany, selectedType, setFilterOption]);
 
-  // fix me! : server에서 가져온 list로 수정 예정
-  const companyList = [
-    { id: '1', value: 'KB국민' },
-    { id: '2', value: '신한' },
-    { id: '3', value: '하나' },
-    { id: '4', value: '농협' },
-  ];
+  const companyList =
+    'companyList' in data
+      ? data?.companyList.map((cur: string, idx: number) => {
+          return { id: `${idx}_cur`, value: cur };
+        })
+      : [];
 
-  const typeList = [
-    { id: '1t', value: '체크' },
-    { id: '2t', value: '신용' },
-  ];
+  const typeList =
+    'typeList' in data
+      ? data?.typeList.map((cur: string, idx: number) => {
+          return { id: `${idx}_cur`, value: cur };
+        })
+      : [];
 
   const handleSubmitSearch = useCallback(
     (e: React.FormEvent<HTMLFormElement>) => {
@@ -40,26 +56,28 @@ export default function ProfitFilter() {
   );
 
   return (
-    <div className="z-10 mt-4 p-4 absolute left-1/2 w-[360px] -translate-x-1/2 rounded-[10px] bg-Secondary flex flex-col justify-between gap-4">
-      <div className="flex justify-start items-center gap-1">
-        <span className="p2r">전체 보기</span>
-        <Switch isOn={isSwitchOn} setIsOn={setIsSwitchOn} />
+    filterOpen && (
+      <div className="z-10 mt-4 p-4 absolute left-1/2 w-[360px] -translate-x-1/2 rounded-[10px] bg-Secondary flex flex-col justify-between gap-4">
+        <div className="flex justify-start items-center gap-1">
+          <span className="p2r">전체 보기</span>
+          <Switch isOn={isSwitchOn} setIsOn={setIsSwitchOn} />
+        </div>
+        <div className="flex justify-between items-center relative z-20">
+          <Dropdown
+            placeholder="카드사"
+            list={companyList || []}
+            selectItem={selectedCompany}
+            setSelectItem={setSelectedCompany}
+          />
+          <Dropdown
+            placeholder="카드 종류"
+            list={typeList || []}
+            selectItem={selectedType}
+            setSelectItem={setSelectedType}
+          />
+        </div>
+        <SearchInputBar onSubmit={handleSubmitSearch} />
       </div>
-      <div className="flex justify-between items-center relative z-20">
-        <Dropdown
-          placeholder="카드사"
-          list={companyList}
-          selectItem={selectedCompany}
-          setSelectItem={setSelectedCompany}
-        />
-        <Dropdown
-          placeholder="카드 종류"
-          list={typeList}
-          selectItem={selectedType}
-          setSelectItem={setSelectedType}
-        />
-      </div>
-      <SearchInputBar onSubmit={handleSubmitSearch} />
-    </div>
+    )
   );
 }

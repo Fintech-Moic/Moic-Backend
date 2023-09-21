e8a620d23b37e422e2eb59bf7e9b19f12367b607
suhwan2004 <suhwan2004@gmail.com>
2023-09-21 16:21:59 +0900

    Feat: cardList 컴포넌트 추가

---
+++ frontend/src/app/profit/molecules/CardList.tsx
@@ -0,0 +1,23 @@
+import CardListItem from '../atoms/CardListItem';
+
+interface CardListProps {
+  list: Array<{
+    company: string;
+    type: string;
+    name: string;
+    cardImage: string;
+  }>;
+  currentPage: number;
+}
+
+export default function CardList({ list, currentPage }: CardListProps) {
+  return (
+    <div className="flex flex-col gap-16 justify-center items-start">
+      {list
+        .slice((currentPage - 1) * 4, Math.min(currentPage * 4, list.length))
+        .map((curItem) => (
+          <CardListItem key={curItem.name} {...curItem} />
+        ))}
+    </div>
+  );
+}

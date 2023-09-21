27beb44829fa33a31d189d4508030383b79c35e8
suhwan2004 <suhwan2004@gmail.com>
2023-09-21 16:22:26 +0900

    Feat: PaginatedCardList 컴포넌트 추가

---
+++ frontend/src/app/profit/organisms/PaginatedCardList.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import { useState } from 'react';
+import CardList from '../molecules/CardList';
+import Pagination from '@/components/molecules/Pagination';
+
+export default function PaginatedCardList({ data }: any) {
+  const [idx, setIdx] = useState(0);
+  return (
+    <div className="flex flex-col items-center h-full pb-6 justify-end gap-8">
+      <CardList list={data.cardList} currentPage={idx + 1} />
+      <Pagination
+        idx={idx}
+        setIdx={setIdx}
+        pageLength={Math.round(
+          (
+            data.cardList as Array<{
+              cardImage: string;
+              company: string;
+              mine: boolean;
+              name: string;
+              type: string;
+            }>
+          ).length / 4
+        )}
+      />
+    </div>
+  );
+}

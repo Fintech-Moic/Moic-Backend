a24a90cbf2f6af33493cbe9f9ffe4e4936a278bc
suhwan2004 <suhwan2004@gmail.com>
2023-09-22 15:49:41 +0900

    Feat: CardList 내에 router 이벤트 추가

--- frontend/src/app/profit/molecules/CardList.tsx
+++ frontend/src/app/profit/molecules/CardList.tsx
@@ -1,3 +1,6 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
 import CardListItem from '../atoms/CardListItem';
 
 interface CardListProps {
@@ -12,8 +15,18 @@ interface CardListProps {
 }
 
 export default function CardList({ list, currentPage }: CardListProps) {
+  const router = useRouter();
+  const handleClickCardItem = (e: React.MouseEvent<HTMLDivElement>) => {
+    const closestCardItem = (e.target as HTMLDivElement).closest('div');
+    if (!closestCardItem) return;
+    router.push(`/profit/card/detail/${closestCardItem.id}`);
+  };
   return (
-    <div className="flex flex-col gap-16 justify-center items-start">
+    <div
+      className="flex flex-col gap-16 justify-center items-start"
+      onClick={handleClickCardItem}
+      aria-hidden="true"
+    >
       {list
         .slice((currentPage - 1) * 4, Math.min(currentPage * 4, list.length))
         .map((curItem) => (

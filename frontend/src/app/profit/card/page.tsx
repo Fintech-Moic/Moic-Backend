7a20ecdc400464e77758aebe89452e2fd7e72398
suhwan2004 <suhwan2004@gmail.com>
2023-09-25 02:23:38 +0900

    Feat: profit/card 페이지 내 suspense 제거

--- frontend/src/app/profit/card/page.tsx
+++ frontend/src/app/profit/card/page.tsx
@@ -1,4 +1,3 @@
-import { Suspense } from 'react';
 import PaginatedCardList from '../organisms/PaginatedCardList';
 import ProfitFilter from '../organisms/ProfitFilter';
 import getAllCard from '@/api/card';
@@ -7,9 +6,9 @@ export default async function Page() {
   const searchOption = await getAllCard();
 
   return (
-    <Suspense fallback={<div>감동의 로딩</div>}>
+    <>
       <ProfitFilter data={searchOption.data} />
       <PaginatedCardList listType="read" />
-    </Suspense>
+    </>
   );
 }

d66e2805025c29361f800465d5fd087bebd9aca6
suhwan2004 <suhwan2004@gmail.com>
2023-09-27 00:08:18 +0900

    Feat: Voc 페이지 구현

--- frontend/src/app/myPage/voc/page.tsx
+++ frontend/src/app/myPage/voc/page.tsx
@@ -1,13 +1,13 @@
-import TextArea from '../atoms/TextArea';
+import VocContainer from '../organisms/VocContainer';
 import Header from '@/components/molecules/Header';
 import Navbar from '@/components/molecules/Navbar';
 
-export default function VOCPage() {
+export default function VocPage() {
   return (
     <>
       <Header title="전체 카드 조회" isFilterButton isPrevButton />
-      <div className="relative flex-1 overflow-y-auto">
-        <TextArea />
+      <div className="relative flex-1 overflow-y-auto flex flex-col items-center w-full">
+        <VocContainer />
       </div>
       <div className="flex justify-center">
         <Navbar />

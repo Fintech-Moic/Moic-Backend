57bcc8e7d6aac00ffc242040fb063023ce713db7
suhwan2004 <suhwan2004@gmail.com>
2023-09-27 00:11:19 +0900

    Feat: Voc 등록 request function 내 body 수정

--- frontend/src/api/myPage.ts
+++ frontend/src/api/myPage.ts
@@ -1,11 +1,11 @@
-export default async function postVocSuggest(text: string) {
+export default async function postVocSuggest(content: string) {
   const response = await fetch('https://moic.site/api/v1/voc', {
     method: 'POST',
     headers: {
       'Content-type': 'application/json',
     },
     credentials: 'include',
-    body: JSON.stringify({ text }),
+    body: JSON.stringify({ id: 'text123', content }),
   });
   const result = await response.json();
   return result;

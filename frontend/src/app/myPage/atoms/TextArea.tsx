d773041d9fa130aaf8b4475cd350b5e8b1c65411
suhwan2004 <suhwan2004@gmail.com>
2023-09-27 00:06:45 +0900

    Feat: Voc 내 사용되는 TextArea 컴포넌트 추가

--- frontend/src/app/myPage/atoms/TextArea.tsx
+++ frontend/src/app/myPage/atoms/TextArea.tsx
@@ -1,16 +1,27 @@
 'use client';
 
-import { forwardRef } from 'react';
+import { forwardRef, ForwardedRef } from 'react';
 
-const TextArea = forwardRef<HTMLDivElement>((ref) => {
-  console.log(ref);
+/** ForwardRef를 통해 부모 컴포넌트에서 value를 가져올 수 있게 해주는 Text Area 컴포넌트
+ * @param {String} placeholder 글씨가 입력되어 있지 않을 때의 안내 문구
+ * @param {number} placeholder TextArea의 최대 가능 입력 글자 수
+ * @returns {JSX.Element} 컴포넌트 반환
+ */
+interface TextAreaProps {
+  placeholder: string;
+  maxLength: number;
+}
+function TextArea(
+  props: TextAreaProps,
+  ref: ForwardedRef<HTMLTextAreaElement>
+) {
   return (
     <textarea
       className="box-border p-3 border-[1px] border-black border-opacity-50 resize-none outline-none rounded-md w-[320px] h-[400px] text-black"
-      placeholder="모익을 사용하셨을 때, 느꼈던 불편한 점, 개선할 점, 관리자에게 문의할 점을 편하게 적어주세요.(최대 5700자)"
-      maxLength={5700}
+      {...props}
+      ref={ref}
     />
   );
-});
+}
 
-export default TextArea;
+export default forwardRef<HTMLTextAreaElement, TextAreaProps>(TextArea);

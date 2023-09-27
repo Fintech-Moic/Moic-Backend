e296b710ca02382d39f25d266737cb6c3f0eb1aa
suhwan2004 <suhwan2004@gmail.com>
2023-09-27 15:54:11 +0900

    Feat: bookMark 리스트 페이지 생성

--- frontend/src/app/myPage/bookMark/page.tsx
+++ frontend/src/app/myPage/bookMark/page.tsx
@@ -1,1 +1,27 @@
-export default function page() {}
+import BookmarkContainer from '../organisms/BookmarkContainer';
+import Header from '@/components/molecules/Header';
+import Navbar from '@/components/molecules/Navbar';
+
+/*
+			{
+			"category" : "커피전문점",
+			"shopName" : "스타벅스",
+			"shopLocation" : "역삼점",
+			"address" : "도로명 주소",
+			"latitude" : "",
+			"longitude" : "",
+				},
+*/
+export default function page() {
+  return (
+    <>
+      <Header title="전체 카드 조회" isFilterButton isPrevButton />
+      <div className="relative flex-1 overflow-y-auto flex flex-col items-center w-full">
+        <BookmarkContainer />
+      </div>
+      <div className="flex justify-center">
+        <Navbar />
+      </div>
+    </>
+  );
+}

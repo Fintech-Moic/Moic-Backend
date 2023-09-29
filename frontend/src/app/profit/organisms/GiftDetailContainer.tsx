b9e2fa2cdb8651f9d756da6c41dd29a5edc4f060
suhwan2004 <suhwan2004@gmail.com>
2023-09-30 04:09:22 +0900

    Feat: 기프티콘 상세 컨테이너 컴포넌트 추가

---
+++ frontend/src/app/profit/organisms/GiftDetailContainer.tsx
@@ -0,0 +1,29 @@
+import { useQuery } from '@tanstack/react-query';
+import { getMyGift } from '@/api/giftCard';
+
+export default function GiftDetailContainer({ giftId }: { giftId: string }) {
+  const { data: giftData, isLoading } = useQuery({
+    queryKey: ['getMyGift'],
+    queryFn: () => getMyGift(),
+  });
+
+  if (isLoading) return <div>loading...</div>;
+
+  const currentGift = giftData.data.find(
+    (curGift: any) => curGift.id === giftId
+  );
+
+  return (
+    <div className="h-full shadow-md flex flex-col justify-center items-center rounded-3xl bg-white">
+      {currentGift ? (
+        <img
+          src={currentGift.imageUrl}
+          alt="기프티콘 이미지"
+          className="w-60 h-96"
+        />
+      ) : (
+        <span>잘못된 접근입니다.</span>
+      )}
+    </div>
+  );
+}

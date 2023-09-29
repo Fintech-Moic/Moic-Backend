3db7e2ff55cab597b420561696f69cfe9dc42a85
김성재 <tjdwo6269@naver.com>
2023-09-29 15:54:13 +0900

    Feat: 기프티콘 목록 조회 api 작성

--- Backend/src/main/java/com/finp/moic/giftCard/model/repository/querydsl/GiftcardRepositoryCustom.java
+++ Backend/src/main/java/com/finp/moic/giftCard/model/repository/querydsl/GiftcardRepositoryCustom.java
@@ -1,5 +1,6 @@
 package com.finp.moic.giftCard.model.repository.querydsl;
 
+import com.finp.moic.giftCard.model.dto.response.GiftcardListResponseDTO;
 import com.finp.moic.giftCard.model.entity.Giftcard;
 import com.finp.moic.shop.model.dto.response.GiftResponseDTO;
 
@@ -9,4 +10,6 @@ public interface GiftcardRepositoryCustom {
     List<GiftResponseDTO> findAllByShopName(String shopName);
 
     List<String> findAllShopNameByUserId(String userId);
+
+    List<GiftcardListResponseDTO> findAllByUserId(String userId);
 }

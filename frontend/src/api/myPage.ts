699806dfb8000bfdbbcf00e0edf63f94f7aeccb7
suhwan2004 <suhwan2004@gmail.com>
2023-10-01 00:56:10 +0900

    Feat: 북마크 삭제 request function 작성

--- frontend/src/api/myPage.ts
+++ frontend/src/api/myPage.ts
@@ -3,4 +3,7 @@ import { authFetchGet, authFetchPost } from '@/util/api';
 export const postVocSuggest = async (content: string) =>
   authFetchPost({ url: '/voc', data: { content } });
 
-export const getMyBookmark = async () => authFetchGet({ url: '/gift/mygifts' });
+export const getMyBookmark = async () => authFetchGet({ url: '/bkm/lookup' });
+
+export const postBookmarkDelete = async (data: any) =>
+  authFetchPost({ url: 'bkm/delete', data });

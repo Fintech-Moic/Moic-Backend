cef21ad53a26e73ea5c5e218c06c48c2680514d0
suhwan2004 <suhwan2004@gmail.com>
2023-10-01 00:54:06 +0900

    Feat: 북마크 페이지 내 컴포넌트 추가

--- frontend/src/app/myPage/bookMark/page.tsx
+++ frontend/src/app/myPage/bookMark/page.tsx
@@ -1,23 +1,13 @@
-import BookmarkContainer from '../organisms/BookmarkContainer';
+import PaginatedBookmarkList from '../organisms/PaginatedBookmarkList';
 import Header from '@/components/molecules/Header';
 import Navbar from '@/components/molecules/Navbar';
 
-/*
-			{
-			"category" : "커피전문점",
-			"shopName" : "스타벅스",
-			"shopLocation" : "역삼점",
-			"address" : "도로명 주소",
-			"latitude" : "",
-			"longitude" : "",
-				},
-*/
 export default function page() {
   return (
     <>
-      <Header title="전체 카드 조회" isFilterButton isPrevButton />
+      <Header title="북마크 관리" isFilterButton={false} isPrevButton />
       <div className="relative flex-1 overflow-y-auto flex flex-col items-center w-full">
-        <BookmarkContainer />
+        <PaginatedBookmarkList usePage="myPage" />
       </div>
       <div className="flex justify-center">
         <Navbar />

c9eb579229cb41afd634c644a859378c1f8a8513
김태현 <pooiiiu4100@gmail.com>
2023-10-04 15:29:28 +0900

    Fix: 토큰 만료시간 설정 변경

--- Backend/src/main/java/com/finp/moic/util/security/service/JwtService.java
+++ Backend/src/main/java/com/finp/moic/util/security/service/JwtService.java
@@ -19,13 +19,13 @@ public class JwtService {
     private final String secretKey;
 
     //30분
-//    private final long expirationHours = 60 * 30;
-    private final long expirationHours = 30;
+    private final long expirationHours = 60 * 30;
+//    private final long expirationHours = 30;
     private final String issuer;
 
     //30일
-//    private final long refreshTokenExpire = 60 * 60 * 24 * 30;
-    private final long refreshTokenExpire = 60;
+    private final long refreshTokenExpire = 60 * 60 * 24 * 30;
+//    private final long refreshTokenExpire = 60;
 
     public JwtService(
             @Value("${issuer}") String issuer,

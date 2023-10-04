6300bab09b177fe40ff6144101b88392b072f3f2
suhwan2004 <suhwan2004@gmail.com>
2023-10-04 17:55:32 +0900

    Fix: Navbar의 카드 접근 버튼 라우팅 수정

--- frontend/src/components/molecules/Navbar.tsx
+++ frontend/src/components/molecules/Navbar.tsx
@@ -56,7 +56,7 @@ function Navbar() {
         height="h-6"
         src={isIncloudePathname('profit/giftCard') ? gifticonOn : gifticon}
         alt="gifticon"
-        onClick={() => navButtonClick('/profit/giftCard/myGiftCard')}
+        onClick={() => navButtonClick('/profit/giftCard')}
       />
     </div>
   );

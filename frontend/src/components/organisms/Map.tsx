7f5a3be5a87206116440eaab982bf006fc785580
배희진 <dilook0424@naver.com>
2023-10-04 11:10:53 +0900

    Fix: 빌드 에러 --> useEffect 의존성 삭제

--- frontend/src/components/organisms/Map.tsx
+++ frontend/src/components/organisms/Map.tsx
@@ -2,11 +2,11 @@
 
 import { useEffect, useState } from 'react';
 import { Map, MapMarker, useKakaoLoader } from 'react-kakao-maps-sdk';
-import { useAtom } from 'jotai';
+import { useSetAtom } from 'jotai';
 import curLocAtom from '@/store/atoms/curLocAtom';
 
 export default function KakaoMap() {
-  const [curLoc, setCurLoc] = useAtom(curLocAtom);
+  const setCurLoc = useSetAtom(curLocAtom);
   const [state, setState] = useState({
     center: {
       lat: 37.50135,
@@ -59,7 +59,7 @@ export default function KakaoMap() {
 
   useEffect(() => {
     setCurLoc(state.center);
-  }, [state.center]);
+  }, [state.center, setCurLoc]);
 
   if (error) return <div>Error</div>;
 

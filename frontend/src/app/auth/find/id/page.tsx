4917f450951328ecd1ffdf3096d7c730678274ab
Uichan Lee <cksxkr5193@naver.com>
2023-10-05 03:54:04 +0900

    Chore: find/id/page에도 TitleSentence component를 이용해 Title을 구현했습니다.

--- frontend/src/app/auth/find/id/page.tsx
+++ frontend/src/app/auth/find/id/page.tsx
@@ -9,6 +9,7 @@ import FindIdForm from '../../organisms/FindIdForm';
 import FindIdSuccessForm from '../../organisms/FindIdSuccessForm';
 import { findIdApi } from '@/api/auth';
 import { useAlreadySignInChecker } from '@/hooks/useSignInChecker';
+import TitleSentence from '@/components/atoms/TitleSentence';
 
 export default function Page() {
   useAlreadySignInChecker();
@@ -60,8 +61,16 @@ export default function Page() {
     />,
   ];
   return (
-    <div className="h-full flex flex-col justify-start items-center">
-      {findIdContentArr[step]}
+    <div className="h-full flex flex-col">
+      <div className="h-1/4 flex items-center">
+        <TitleSentence
+          title="아이디 찾기"
+          sentence="인증을 통해 계정 정보를 찾아보세요."
+        />
+      </div>
+      <div className="h-full flex flex-col justify-start items-center">
+        {findIdContentArr[step]}
+      </div>
     </div>
   );
 }

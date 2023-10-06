f970f520fef123aecc1c3152e4c3cae57c060cf6
suhwan2004 <suhwan2004@gmail.com>
2023-10-06 09:38:19 +0900

    Fix: 카드가 빌 때의 사진이 보이지 않던 에러 대응 및 수정

--- frontend/src/app/profit/atoms/CardEmptyRegistButton.tsx
+++ frontend/src/app/profit/atoms/CardEmptyRegistButton.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 import { useCallback } from 'react';
 import CardSubTitle from './CardSubTitle';
+import GrayCardIcon from '@/../public/assets/GrayCardIcon.svg';
 
 /** 내 카드 조회 페이지에서, 내 카드가 없을 시 보여주는 컴포넌트
  * @returns {JSX.Element} 컴포넌트 반환
@@ -19,12 +20,7 @@ export default function CardEmptyRegistButton() {
       className="flex flex-col items-center justify-center"
       onClick={handleClick}
     >
-      <Image
-        src="/assets/GrayCardIcon.svg"
-        alt="카드이미지"
-        width={196}
-        height={160}
-      />
+      <Image src={GrayCardIcon} alt="카드이미지" width={196} height={160} />
       <CardSubTitle size="t2" value="카드가 없어요..." />
       <CardSubTitle size="t4" value="카드 등록하러 가기" />
     </button>
